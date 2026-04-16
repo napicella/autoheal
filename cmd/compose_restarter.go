30f623c854e7b3b3d66438d825802e060b06e3b7
Nicola Apicella <napicella4@gmail.com>
2026-04-16 22:27:51 +0200

    Use compose down + up to reboot the compose stack

--- cmd/compose_restarter.go
+++ cmd/compose_restarter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"os/exec"
 )
@@ -18,8 +19,14 @@ func restartCompose(ctx context.Context, proj composeProject) error {
 	// Using the compose CLI instead of the compose go SDK.
 	// The compose SDK still relies on the old docker SDK instead of moby.
 	// When they switch over to moby we can refactor this to use the compose SDK.
-	restartCmd := exec.CommandContext(ctx, "docker", "compose", "-f", proj.filepath, "-p", proj.name, "restart")
-	restartCmd.Stdout = os.Stdout
-	restartCmd.Stderr = os.Stderr
-	return restartCmd.Run()
+	downCmd := exec.CommandContext(ctx, "docker", "compose", "-f", proj.filepath, "-p", proj.name, "down")
+	downCmd.Stdout = os.Stdout
+	downCmd.Stderr = os.Stderr
+	if err := downCmd.Run(); err != nil {
+		return fmt.Errorf("failed to run docker compose down: %w", err)
+	}
+	upCmd := exec.CommandContext(ctx, "docker", "compose", "-f", proj.filepath, "-p", proj.name, "up", "-d")
+	upCmd.Stdout = os.Stdout
+	upCmd.Stderr = os.Stderr
+	return upCmd.Run()
 }
